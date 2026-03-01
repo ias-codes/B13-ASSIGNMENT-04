6f0ab56829eb06a2779a866a3db50eb616490f69
ias-codes <ishtiak400@gmail.com>
2026-03-02 01:03:42 +0600

    STARTED

---
+++ app.js
@@ -0,0 +1,11 @@
+let JobCardsInput = document.querySelectorAll('#jobs-total-card');
+let totalJobsCard = JobCardsInput.length;
+
+//  input total jobs
+let totalJobs = document.getElementById('total-job');
+let totalInterviews = document.getElementById('total-interview');
+let totalRejected = document.getElementById('total-rejected');
+document.getElementById('total-job-right').innerText = totalJobsCard;
+
+totalJobs.innerText = totalJobsCard;
+
